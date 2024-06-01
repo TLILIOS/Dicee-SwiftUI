1554c2699e1cb5fc9429acc93d3709067c613d4e
TLiLi HAMDI <tlilios@yahoo.com>
2024-06-01 11:24:27 +0200

    Starting

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by HTLILI on 01/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
